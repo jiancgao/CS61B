492abec389d4bcec1cb0460356560b2eeb5646a3
Jiancong Gao <jiancong.gao@outlook.com>
2019-03-04 15:47:54 +0800

    update test

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 public class TestOffByOne {
     // You must use this CharacterComparator and not instantiate
     // new ones, or the autograder might be upset.
-    static CharacterComparator offByOne = new OffByOne();
+    private static CharacterComparator offByOne = new OffByOne();
 
     // Your tests go here.
     @Test
