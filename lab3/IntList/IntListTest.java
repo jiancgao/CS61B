aa05b43cfa24554d1fce5d826cc10743642a37f6
Jiancong Gao <jiancong.gao@outlook.com>
2019-03-04 13:39:45 +0800

    finished lab 3

--- lab3/IntList/IntListTest.java
+++ lab3/IntList/IntListTest.java
@@ -66,8 +66,19 @@ public class IntListTest {
         assertEquals(IntList.of(1, 2, 3), A);
     }
 
-    /** If you're running this from the command line, you'll need
-      * to add a main method. See ArithmeticTest.java for an
-      * example. */
+    /**
+     * If you're running this from the command line, you'll need
+     * to add a main method. See ArithmeticTest.java for an
+     * example.
+     */
+
+    @Test
+    public void testReverse() {
+        IntList A = IntList.of(1, 2, 3, 4);
+        IntList Aexp = IntList.of(4, 3, 2, 1);
+        assertEquals(Aexp, IntList.reverse(A));
+        assertEquals(null, IntList.reverse(null));
+
+    }
 
 }
